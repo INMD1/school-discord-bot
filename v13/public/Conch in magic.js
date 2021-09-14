8868baa096269226be76f3699f74eeb837eee6bc
INMD1 <78072355+INMD1@users.noreply.github.com>
2021-09-14 12:09:13 +0900

    update

---
+++ v13/public/Conch in magic.js
@@ -0,0 +1,43 @@
+const Discord = require("discord.js");
+const weather = require("weather-js");
+const { see } = require("../jsonfile/config.json");
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("conch")
+    .setDescription("질문을 하면 대답을 해줘요!")
+    .addStringOption((option) =>
+      option
+        .setName("ask")
+        .setDescription("질문할 내용을 적어주세요. (성의없게 답할수 있습)")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    var ask = [
+      "안돼",
+      "물론",
+      "넌 왜그러니 나한데",
+      "안되요",
+      "그건좀..",
+      "stop!",
+      "다시 해봐",
+      "나도 몰라",
+      "하면 그사람",
+      "그래",
+      "돼",
+      "돌아가",
+      "일이나 하세요^^",
+    ];
+    var askuNum = Math.floor(Math.random() * 13);
+    var result = ask[askuNum];
+    console.log("정상적으로 답변을 보냄(소라고동 명렁어 부분)");
+    const send = new Discord.MessageEmbed()
+      .setColor("#DEB887")
+      .setTitle("마법에 소라고동")
+      .setDescription(result)
+      .setTimestamp();
+      interaction.reply({embeds: [send]});
+  },
+};
